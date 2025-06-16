cdae71bd9cd3208e884f7e766652a9647a5db2fd
Wendel WSL Ubuntu <wfrsilva@gmail.com>
2025-06-16 19:08:49 -0300

    Primeiro commit do projeto Spring Boot

---
+++ src/main/java/dev/wfrsilva/crud_spring_boot_2025/CrudSpringBoot2025Application.java
@@ -0,0 +1,13 @@
+package dev.wfrsilva.crud_spring_boot_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBoot2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBoot2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wfrsilva/crud_spring_boot_2025/CrudSpringBoot2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wfrsilva.crud_spring_boot_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBoot2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
